1838f36eb63e1d76d6dc50ff99b91b496a0cd96e
AlesLisovski <aleslisovskij@gmail.com>
2024-01-19 20:43:12 +0300

    Update test.cpp

--- Second course/Operation-systems/Lab3(Thread synchronization)/Tests/test.cpp
+++ Second course/Operation-systems/Lab3(Thread synchronization)/Tests/test.cpp
@@ -1,47 +1,86 @@
 #include "pch.h"
-#include "C:\Users\alesl\OneDrive\Рабочий стол\Новая папка\Lab2-Test\TestSuite\funcToTest.cpp" 
+#include "CppUnitTest.h"
+#include "C:\Users\alesl\OneDrive\Рабочий стол\Новая папка\UnitTest1\TestSuite\TestSuite.cpp"
 
-TEST(MinMaxTest, BasicTest) {
-	std::vector<int> vec = { 3, 1, 4, 1, 5, 9 };
-	int min, max;
-	min_max(min, max, vec);
-	ASSERT_EQ(min, 1);
-	ASSERT_EQ(max, 9);
-}
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-TEST(MinMaxTest, EmptyVectorTest) {
-	std::vector<int> vec;
-	int min, max;
-	min_max(min, max, vec);
-	ASSERT_EQ(min, 0); 
-	ASSERT_EQ(max, 0);
-}
+namespace UnitTest1
+{
+	TEST_CLASS(CountMarkedElementsTest)
+	{
+	public:
 
-TEST(MinMaxTest, MinMaxSameValueTest) {
-	std::vector<int> vec = { 3, 3, 3, 3 };
-	int min, max;
-	min_max(min, max, vec);
-	ASSERT_EQ(min, 3);
-	ASSERT_EQ(max, 3);
-}
+		TEST_METHOD(BasicTest)
+		{
+			std::vector<int> arr = { 1, 2, 3, 4, 5, 4, 4, 6 };
+			int value = 4;
+			int result = count_marked_elements(arr, value);
+			Assert::AreEqual(3, result);
+		}
 
-TEST(AverageTest, BasicTest) {
-	std::vector<int> vec = { 3, 1, 4, 1, 5, 9 };
-	int average = 0;
-	Average(average, vec);
-	ASSERT_EQ(average, 3);
-}
+		TEST_METHOD(EmptyArrayTest)
+		{
+			std::vector<int> arr;
+			int value = 4;
+			int result = count_marked_elements(arr, value);
+			Assert::AreEqual(0, result);
+		}
 
-TEST(AverageTest, EmptyVectorTest) {
-	std::vector<int> vec;
-	int average = 0;
-	Average(average, vec);
-	ASSERT_EQ(average, 0);
-}
+		TEST_METHOD(NoMarkedElementsTest)
+		{
+			std::vector<int> arr = { 1, 2, 3, 5, 6 };
+			int value = 4;
+			int result = count_marked_elements(arr, value);
+			Assert::AreEqual(0, result);
+		}
 
-TEST(AverageTest, LargeVectorTest) {
-	std::vector<int> vec(10000, 5); 
-	int average = 0;
-	Average(average, vec);
-	ASSERT_EQ(average, 5); 
+		TEST_METHOD(AllMarkedElementsTest)
+		{
+			std::vector<int> arr = { 4, 4, 4, 4 };
+			int value = 4;
+			int result = count_marked_elements(arr, value);
+			Assert::AreEqual(4, result);
+		}
+	};
+
+	TEST_CLASS(ThreadDeleteResultTest)
+	{
+	public:
+
+		TEST_METHOD(BasicTest)
+		{
+			std::vector<int> arr = { 1, 2, 3, 4, 5, 4, 4, 6 };
+			int value = 4;
+			std::vector<int> result = thread_delete_result(arr, value);
+			std::vector<int> expected = { 1, 2, 3, 0, 5, 0, 0, 6 };
+			Assert::IsTrue(expected ==  result);
+		}
+
+		TEST_METHOD(EmptyArrayTest)
+		{
+			std::vector<int> arr;
+			int value = 4;
+			std::vector<int> result = thread_delete_result(arr, value);
+			std::vector<int> expected;
+			Assert::IsTrue(expected == result);
+		}
+
+		TEST_METHOD(NoMarkedElementsTest)
+		{
+			std::vector<int> arr = { 1, 2, 3, 5, 6 };
+			int value = 4;
+			std::vector<int> result = thread_delete_result(arr, value);
+			std::vector<int> expected = { 1, 2, 3, 5, 6 };
+			Assert::IsTrue(expected == result);
+		}
+
+		TEST_METHOD(AllMarkedElementsTest)
+		{
+			std::vector<int> arr = { 4, 4, 4 };
+			int value = 4;
+			std::vector<int> result = thread_delete_result(arr, value);
+			std::vector<int> expected = { 0, 0, 0 };
+			Assert::IsTrue(expected == result);
+		}
+	};
 }
